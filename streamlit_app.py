9ed87466dabf2241571e88ae4ac3d1d780ee4afc
przygm <47592807+przygm@users.noreply.github.com>
2026-03-21 23:02:22 +0100

    Disable smoothie order functionality
    Comment out smoothie order submission and response display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,14 +45,15 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
 
-    import requests  
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response.json())
+import requests  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    time_to_insert = st.button('Submit Order')
+#    time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+#    if time_to_insert:
+#        session.sql(my_insert_stmt).collect()
+#        st.success('Your Smoothie is ordered!', icon="✅")
 
   
