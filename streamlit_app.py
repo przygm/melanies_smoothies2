c316862c3ca719fcf2ad51ad0e43c0dffd7d908a
przygm <47592807+przygm@users.noreply.github.com>
2026-03-21 23:05:31 +0100

    Fetch and display watermelon data in the app
    Added a request to fetch watermelon data from an API and display it in the app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests  
 
 # Write directly to the app.
 st.title(f"🥤Customize your smoothie🥤")
@@ -36,19 +37,18 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)    
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                    values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+    #                values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
     #st.write(my_insert_stmt)
     #st.stop()
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 #    time_to_insert = st.button('Submit Order')
 
