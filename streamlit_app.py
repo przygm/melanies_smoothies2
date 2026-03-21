838ed8abd1b7870094a87c1e54f6c723f9d6c794
przygm <47592807+przygm@users.noreply.github.com>
2026-03-21 23:31:34 +0100

    Uncomment order submission logic in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,10 +62,10 @@ if ingredients_list:
     #st.stop()
 
 
-#    time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
 
-#    if time_to_insert:
-#        session.sql(my_insert_stmt).collect()
-#        st.success('Your Smoothie is ordered!', icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
 
   
