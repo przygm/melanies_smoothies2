48c7fc12699e43938fb1cc22e28fd2c5572ef598
przygm <47592807+przygm@users.noreply.github.com>
2026-03-21 22:57:33 +0100

    Refactor import and request for watermelon API

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,9 @@ if ingredients_list:
     #st.write(my_insert_stmt)
     #st.stop()
 
-    import requests  
-    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-    st.text(smoothiefroot_response)
+import requests  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
     time_to_insert = st.button('Submit Order')
 
