2d74a76b530e02af69bd69b32bc1f1e5543a9b65
przygm <47592807+przygm@users.noreply.github.com>
2026-03-21 23:21:16 +0100

    Refactor dataframe handling in streamlit_app.py
    Commented out the original dataframe display and added conversion to Pandas dataframe for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,15 @@ st.write('The name of your smoothie will be: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 #editable_df = st.data_editor(my_dataframe)
 #st.dataframe(data=my_dataframe, use_container_width=True) 
 
+#convert the SnowPark dataframe to a Pandas dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
